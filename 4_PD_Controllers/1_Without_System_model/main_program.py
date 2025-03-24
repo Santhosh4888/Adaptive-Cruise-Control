1606472773db800108f1622d38697f8e9b79c809
Kannikeshwwar K <kannikeshwwark@gmail.com>
2025-03-24 17:08:05 +0530

    Adding all

---
+++ 4_PD_Controllers/1_Without_System_model/main_program.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import Controller
+import Kinematics
+import sys
+CP_module_dir = r'D:\lord_of_darkness\IITM\5th_year\DDP_Final\6_Common_Paramaters'
+sys.path.append(CP_module_dir)
+import Common_Params as CP
+
+def main(kp):
+
+    store_seperations = [[] for _ in range(len(CP.Drs))]                   # For storing the seperations for different cases
+    store_acceleration = [[] for _ in range(len(CP.Drs))]                  # For storing the acceleration input for different cases
+    store_velocities = [[] for _ in range(len(CP.Drs))]                    # For storing the velocities of ego vehicle for different cases
+    store_time = [[] for _ in range(len(CP.Drs))]                          # For storing the time taken for plotting purposes
+    
+    store_detected_times = [None for _ in range(len(CP.Drs))]              # Stores the time at which the obstacle is being detected
+    store_start_reaction_times = [None for _ in range(len(CP.Drs))]        # Stores the time at which the vehicle starts to react
+    
+    for i in range(len(CP.Drs)):
+        upper_controller = Controller.PID_Controller()                     # The Upper Controller
+        upper_controller.set_proportional(kp)                              # Set proportional and derivative contoller constants        
+        
+        if i == 0:
+            upper_controller.desired_speed = 20 * 5 / 18                   # Desired velocity of ego vehicle in [km/hr * (5/18) = m/s]
+        else:
+            upper_controller.desired_speed = CP.ego_max_v
+
+        ego_value = CP.start_ego_values
+        ego = Kinematics.kinematics_1()                                    # For setting the kinematics equations of the ego vehicle 
+        if i == 0:
+            pre = Kinematics.kinematics_2(CP.Drs[i] + 10)                  # For setting the kinematics equations of the preceeding vehicle
+        else:
+            pre = Kinematics.kinematics_2(CP.Drs[i] + 25)                  # For setting the kinematics equations of the preceeding vehicle
+
+        ego_values = ego.update_velocity(ego_value)
+        preceeding_values = pre.update_velocity(CP.Vps[i])
+
+        time = 0.0                                                         # Start time 0.0 seconds
+        detected_time = 0.0                                                # Time for storing the detected time
+        detected = 'Not detected'                                          # Boolean to store wether the vehicle has detected the obstacle or not
+        while time < CP.total_experiment_time:
+
+            store_velocities[i].append(ego_values[1] * 18 / 5)                                                    # Stores ego vehicle velocities in km/hr
+            store_seperations[i].append(preceeding_values[0] - ego_values[0])                                     # Stores the current seperation
+            store_time[i].append(time)    
+
+            if preceeding_values[0] - ego_values[0] > CP.Drs[i] and detected == 'Not detected':
+                preceeding_values = None
+                
+            elif detected == 'Not detected':
+                preceeding_values = None
+                detected = 'first'
+                store_detected_times[i] = time
+            
+            elif detected == 'first':
+                preceeding_values = None
+            
+            a = upper_controller.get_acceleration(ego_values = ego_values, preceeding_values = preceeding_values) # Get the control input
+            
+            store_acceleration[i].append(a)                                                                       # Stores the acceleration values                                                                         # Store the time
+
+            ego_values = ego.control(a)
+            preceeding_values = pre.control()
+            
+            time += CP.sample_time
+            
+            if detected == 'first':
+                detected_time += CP.sample_time
+            else:
+                detected_time = 0
+            
+            if detected_time > CP.Tbl and detected == 'first':
+                detected = 'second'
+                store_start_reaction_times[i] = time
+    
+    return [store_seperations, store_acceleration, store_velocities, store_time, store_detected_times, store_start_reaction_times]
+            
+if __name__ == '__main__':
+    store_seperations, store_acceleration, store_velocities, store_time = main(0.0001)
+    print("Program ended")
---
+++ 8_Hardware/Constant_throttle_pot/Constant_throttle_pot.ino
@@ -0,0 +1,683 @@
+#include <Arduino_CAN.h>
+
+#define CAN_BITRATE CanBitRate::BR_500k
+#define CAN_ID 0x626
+
+//#define OD_MaxSpeedLim  0x3559
+#define OD_MaxSpeed     0x3840
+#define OD_MaxSpCont    0x3559
+#define OD_AccelRate    0x3843
+#define OD_DecelRate    0x3843
+#define OD_BrakeRate    0x3848
+#define OD_DriveCurrL   0x305B
+#define OD_RegenCurrL   0x305C
+#define OD_ForwardDB    0x3001
+#define OD_ForwardMap   0x3002
+#define OD_ForwardMax   0x3003
+#define OD_ForwardOff   0x3004
+#define OD_SpeedToRPM   0x304C
+//#define OD_ThrFilter    0x3030
+
+#define OD_MotorRPM     0x3207
+#define OD_MasterTime   0x3160
+#define OD_ThrottlePot  0x3215
+#define OD_ThrottleCmd  0x3216
+#define OD_CurrentRMS   0x3209
+#define OD_RegenState   0x322B
+#define OD_VehSpeed     0x320A
+#define OD_VehDist      0x3616
+#define OD_VehAccel     0x35C1
+
+uint16_t const read_indices[] = {OD_MaxSpeed, OD_MaxSpCont, OD_AccelRate, OD_DecelRate, OD_BrakeRate, OD_DriveCurrL, OD_RegenCurrL, OD_ForwardDB, OD_ForwardMap, OD_ForwardMax, OD_ForwardOff, OD_SpeedToRPM};
+float const readindices_scaling[] = {1, 1, 30.0/3000, 30.0/30000 ,30.0/30000 ,100.0/32767 ,100.0/32767 ,5.0/32767, 100.0/32767, 5.0/32767, 100.0/32767, 0.1};
+int const readindices_length = sizeof(read_indices)/sizeof(read_indices[0]);
+float readindices_values[readindices_length] = {};
+
+uint16_t const monitor_indices[] = {OD_MotorRPM, OD_ThrottlePot, OD_ThrottleCmd, OD_CurrentRMS, OD_RegenState, OD_VehSpeed, OD_VehDist, OD_VehAccel, OD_MasterTime};
+float const monitorindices_scaling[] = {1, 5.5/36044, 100.0/32767, 0.1, 1, 0.1, 0.1, 10.0/10000, 0.1};
+int const monitorindices_length = sizeof(monitor_indices)/sizeof(monitor_indices[0]);
+float monitorindices_values[monitorindices_length] = {};
+
+float desired_voltage = 0;        // Input will be obtained from the user
+bool targetReached = false;
+
+float max_rpm = readindices_values[0];
+float deadband_voltage = readindices_values[7];
+float map_setting = readindices_values[8];
+float voltage_max = readindices_values[9];
+float offset = readindices_values[10];
+
+//Implement forward/reverse mode select using Arduino before doing braking test using reverse command
+bool reverseTest = false;
+float reverse_voltage = 1;
+
+unsigned long start_time = 0xFFFFFFFF;           // Variable to store the start time, The initialized value is sentinal value indicating 'Not Initialized'
+unsigned long test_time = 50000;                 // Sends throttle pot signal to the motor controller for this time (in milliseconds)
+
+void clear_receive_buffer() {
+  while(CAN.available()) {
+    CAN.read();
+  }
+}
+
+int sdo_download(uint16_t odvalue, uint16_t datavalue) {
+  uint8_t firstbyte = odvalue % 256;
+  uint8_t secondbyte = odvalue / 256;
+
+  uint8_t databyte1 = datavalue % 256;
+  uint8_t databyte2 = datavalue / 256;
+
+  uint8_t const msg_data[] = {0x20, firstbyte, secondbyte, 0x00, databyte1, databyte2, 0, 0};
+
+  CanMsg const msg(CanStandardId(CAN_ID), sizeof(msg_data), msg_data);
+  int const rc = CAN.write(msg);
+
+  if(rc < 0) {
+    Serial.print("Upload failed with error code: ");
+    Serial.println(rc);
+    return rc;
+  }
+
+  //Checking
+  while(true) {
+    if(CAN.available()) {
+      CanMsg const ackmsg = CAN.read();
+      uint8_t readbyte1 = ackmsg.data[4];
+      uint8_t readbyte2 = ackmsg.data[5];
+      short readvalue = ((readbyte2 << 8) | readbyte1);
+
+      uint8_t indexbyte1 = ackmsg.data[1];
+      uint8_t indexbyte2 = ackmsg.data[2];
+      uint16_t odindex = ((indexbyte2 << 8) | indexbyte1);
+
+      if(odindex == odvalue && readvalue == 0) {
+        Serial.print("Successfully changed: ");
+        Serial.print(odindex);
+        Serial.print(" => ");
+        Serial.println(datavalue);
+        return 1;
+      }
+      else {
+        Serial.println("Error in updating value");
+        Serial.println(ackmsg);
+        return 0;
+      }
+      break;
+    }
+  }
+
+  return 0;
+}
+
+int send_sdo_upload(uint16_t odvalue) {
+
+  uint8_t firstbyte = odvalue % 256;
+  uint8_t secondbyte = odvalue / 256;
+
+  uint8_t const msg_data[] = {0x40, firstbyte, secondbyte, 0x00, 0, 0, 0, 0};
+  CanMsg const msg(CanStandardId(CAN_ID), sizeof(msg_data), msg_data);
+  int const rc = CAN.write(msg);
+
+  if(rc < 0) {
+    Serial.print("Upload failed with error code: ");
+    Serial.println(rc);
+  }
+
+  return rc;
+
+}
+
+void receive_sdo_upload() {
+
+  CanMsg const msg = CAN.read();
+
+  if(msg.id == 0x726) {   //do nothing if heartbeat
+    return;
+  }
+
+  uint8_t databytes[] = {msg.data[4], msg.data[5], msg.data[6], msg.data[7]};
+  int datavalue = databytes[3] << 24 | databytes[2] << 16 | databytes[1] << 8 | databytes[0];    //bitshift and concatenate; using int for accurate mapping from unsigned 32-bit to signed
+
+  uint8_t indexbyte1 = msg.data[1];
+  uint8_t indexbyte2 = msg.data[2];
+  uint16_t odindex = ((indexbyte2 << 8) | indexbyte1); 
+
+  for(int i=0; i < readindices_length; i++) {
+    if(odindex == read_indices[i]) {
+      readindices_values[i] = readindices_scaling[i]*datavalue;
+    }
+  }
+
+}
+
+void receive_sdo_upload_monitor() {
+
+  CanMsg const msg = CAN.read();
+
+  if(msg.id == 0x726) {   //do nothing if heartbeat
+    return;
+  }
+
+  uint8_t databyte1 = msg.data[4];
+  uint8_t databytes[] = {msg.data[4], msg.data[5], msg.data[6], msg.data[7]};
+  int datavalue = databytes[3] << 24 | databytes[2] << 16 | databytes[1] << 8 | databytes[0];    //bitshift and concatenate; using int for accurate mapping from unsigned 32-bit to signed
+
+  uint8_t indexbyte1 = msg.data[1];
+  uint8_t indexbyte2 = msg.data[2];
+  uint16_t odindex = ((indexbyte2 << 8) | indexbyte1); 
+
+  for(int i=0; i < monitorindices_length; i++) {
+    if(odindex == monitor_indices[i]) {
+      monitorindices_values[i] = monitorindices_scaling[i]*datavalue;
+    }
+  }
+
+}
+
+void read_params() {
+  int rc = 0;
+  CAN.read();
+  for(int i=0; i < readindices_length; i++) {
+    send_sdo_upload(read_indices[i]);
+    delay(5);
+
+    while(!CAN.available());
+    while(CAN.available()) {
+      receive_sdo_upload();
+    }
+    delay(5);
+  }
+
+  for(int i=0; i < readindices_length; i++) {
+    Serial.print(readindices_values[i]);
+    Serial.print(", ");
+  }
+
+  max_rpm = readindices_values[0];
+  deadband_voltage = readindices_values[7];
+  map_setting = readindices_values[8];
+  voltage_max = readindices_values[9];
+  offset = readindices_values[10];
+
+  if(max_rpm == -1 || deadband_voltage == -1 || map_setting == -1 || voltage_max == -1 || offset == -1) {
+    Serial.println("All parameters not read");
+    while(true);
+  }
+  
+}
+
+
+void setup() { 
+  pinMode(A0, OUTPUT);
+  Serial.begin(115200);
+
+  if (!CAN.begin(CAN_BITRATE))
+  {
+    Serial.println("CAN.begin(...) failed.");
+    for (;;) {}
+  }
+  delay(2000);
+  Serial.println();
+
+  Serial.println("Setting up parameters");
+  clear_receive_buffer();
+  if(int rc = sdo_download(OD_AccelRate, 1000) < 0) {     
+    Serial.println("SDO download failed");
+    for(;;) {}
+  }
+  delay(10);
+  if(int rc = sdo_download(OD_DecelRate, 100) < 0) {     
+    Serial.println("SDO download failed");
+    for(;;) {}
+  }
+  delay(10);
+  if(int rc = sdo_download(OD_BrakeRate, 100) < 0) {     
+  Serial.println("SDO download failed");
+  for(;;) {}
+  }
+
+  delay(2000);
+  Serial.println();
+  Serial.println("1: Read parameters");
+  Serial.println("2: Start test");
+
+  int choice;
+
+  while(Serial.available() <= 0);
+  if(Serial.available()>0) {
+    choice = Serial.parseInt();
+    Serial.parseInt();
+  }
+  
+  switch(choice) {
+    case 1:
+    delay(1000);
+    read_params();
+    while(true);#include <Arduino_CAN.h>
+
+#define CAN_BITRATE CanBitRate::BR_500k
+#define CAN_ID 0x626
+
+//#define OD_MaxSpeedLim  0x3559
+#define OD_MaxSpeed     0x3840
+#define OD_MaxSpCont    0x3559
+#define OD_AccelRate    0x3843
+#define OD_DecelRate    0x3843
+#define OD_BrakeRate    0x3848
+#define OD_DriveCurrL   0x305B
+#define OD_RegenCurrL   0x305C
+#define OD_ForwardDB    0x3001
+#define OD_ForwardMap   0x3002
+#define OD_ForwardMax   0x3003
+#define OD_ForwardOff   0x3004
+#define OD_SpeedToRPM   0x304C
+//#define OD_ThrFilter    0x3030
+
+#define OD_MotorRPM     0x3207
+#define OD_MasterTime   0x3160
+#define OD_ThrottlePot  0x3215
+#define OD_ThrottleCmd  0x3216
+#define OD_CurrentRMS   0x3209
+#define OD_RegenState   0x322B
+#define OD_VehSpeed     0x320A
+#define OD_VehDist      0x3616
+#define OD_VehAccel     0x35C1
+
+uint16_t const read_indices[] = {OD_MaxSpeed, OD_MaxSpCont, OD_AccelRate, OD_DecelRate, OD_BrakeRate, OD_DriveCurrL, OD_RegenCurrL, OD_ForwardDB, OD_ForwardMap, OD_ForwardMax, OD_ForwardOff, OD_SpeedToRPM};
+float const readindices_scaling[] = {1, 1, 30.0/3000, 30.0/30000 ,30.0/30000 ,100.0/32767 ,100.0/32767 ,5.0/32767, 100.0/32767, 5.0/32767, 100.0/32767, 0.1};
+int const readindices_length = sizeof(read_indices)/sizeof(read_indices[0]);
+float readindices_values[readindices_length] = {};
+
+uint16_t const monitor_indices[] = {OD_MotorRPM, OD_ThrottlePot, OD_ThrottleCmd, OD_CurrentRMS, OD_RegenState, OD_VehSpeed, OD_VehDist, OD_VehAccel, OD_MasterTime};
+float const monitorindices_scaling[] = {1, 5.5/36044, 100.0/32767, 0.1, 1, 0.1, 0.1, 10.0/10000, 0.1};
+int const monitorindices_length = sizeof(monitor_indices)/sizeof(monitor_indices[0]);
+float monitorindices_values[monitorindices_length] = {};
+
+float desired_voltage = 0;        // Input will be obtained from the user
+bool targetReached = false;
+
+float max_rpm = readindices_values[0];
+float deadband_voltage = readindices_values[7];
+float map_setting = readindices_values[8];
+float voltage_max = readindices_values[9];
+float offset = readindices_values[10];
+
+//Implement forward/reverse mode select using Arduino before doing braking test using reverse command
+bool reverseTest = false;
+float reverse_voltage = 1;
+
+unsigned long start_time = 0xFFFFFFFF;           // Variable to store the start time, The initialized value is sentinal value indicating 'Not Initialized'
+unsigned long test_time = 50000;                 // Sends throttle pot signal to the motor controller for this time (in milliseconds)
+
+void clear_receive_buffer() {
+  while(CAN.available()) {
+    CAN.read();
+  }
+}
+
+int sdo_download(uint16_t odvalue, uint16_t datavalue) {
+  uint8_t firstbyte = odvalue % 256;
+  uint8_t secondbyte = odvalue / 256;
+
+  uint8_t databyte1 = datavalue % 256;
+  uint8_t databyte2 = datavalue / 256;
+
+  uint8_t const msg_data[] = {0x20, firstbyte, secondbyte, 0x00, databyte1, databyte2, 0, 0};
+
+  CanMsg const msg(CanStandardId(CAN_ID), sizeof(msg_data), msg_data);
+  int const rc = CAN.write(msg);
+
+  if(rc < 0) {
+    Serial.print("Upload failed with error code: ");
+    Serial.println(rc);
+    return rc;
+  }
+
+  //Checking
+  while(true) {
+    if(CAN.available()) {
+      CanMsg const ackmsg = CAN.read();
+      uint8_t readbyte1 = ackmsg.data[4];
+      uint8_t readbyte2 = ackmsg.data[5];
+      short readvalue = ((readbyte2 << 8) | readbyte1);
+
+      uint8_t indexbyte1 = ackmsg.data[1];
+      uint8_t indexbyte2 = ackmsg.data[2];
+      uint16_t odindex = ((indexbyte2 << 8) | indexbyte1);
+
+      if(odindex == odvalue && readvalue == 0) {
+        Serial.print("Successfully changed: ");
+        Serial.print(odindex);
+        Serial.print(" => ");
+        Serial.println(datavalue);
+        return 1;
+      }
+      else {
+        Serial.println("Error in updating value");
+        Serial.println(ackmsg);
+        return 0;
+      }
+      break;
+    }
+  }
+
+  return 0;
+}
+
+int send_sdo_upload(uint16_t odvalue) {
+
+  uint8_t firstbyte = odvalue % 256;
+  uint8_t secondbyte = odvalue / 256;
+
+  uint8_t const msg_data[] = {0x40, firstbyte, secondbyte, 0x00, 0, 0, 0, 0};
+  CanMsg const msg(CanStandardId(CAN_ID), sizeof(msg_data), msg_data);
+  int const rc = CAN.write(msg);
+
+  if(rc < 0) {
+    Serial.print("Upload failed with error code: ");
+    Serial.println(rc);
+  }
+
+  return rc;
+
+}
+
+void receive_sdo_upload() {
+
+  CanMsg const msg = CAN.read();
+
+  if(msg.id == 0x726) {   //do nothing if heartbeat
+    return;
+  }
+
+  uint8_t databytes[] = {msg.data[4], msg.data[5], msg.data[6], msg.data[7]};
+  int datavalue = databytes[3] << 24 | databytes[2] << 16 | databytes[1] << 8 | databytes[0];    //bitshift and concatenate; using int for accurate mapping from unsigned 32-bit to signed
+
+  uint8_t indexbyte1 = msg.data[1];
+  uint8_t indexbyte2 = msg.data[2];
+  uint16_t odindex = ((indexbyte2 << 8) | indexbyte1); 
+
+  for(int i=0; i < readindices_length; i++) {
+    if(odindex == read_indices[i]) {
+      readindices_values[i] = readindices_scaling[i]*datavalue;
+    }
+  }
+
+}
+
+void receive_sdo_upload_monitor() {
+
+  CanMsg const msg = CAN.read();
+
+  if(msg.id == 0x726) {   //do nothing if heartbeat
+    return;
+  }
+
+  uint8_t databyte1 = msg.data[4];
+  uint8_t databytes[] = {msg.data[4], msg.data[5], msg.data[6], msg.data[7]};
+  int datavalue = databytes[3] << 24 | databytes[2] << 16 | databytes[1] << 8 | databytes[0];    //bitshift and concatenate; using int for accurate mapping from unsigned 32-bit to signed
+
+  uint8_t indexbyte1 = msg.data[1];
+  uint8_t indexbyte2 = msg.data[2];
+  uint16_t odindex = ((indexbyte2 << 8) | indexbyte1); 
+
+  for(int i=0; i < monitorindices_length; i++) {
+    if(odindex == monitor_indices[i]) {
+      monitorindices_values[i] = monitorindices_scaling[i]*datavalue;
+    }
+  }
+
+}
+
+void read_params() {
+  int rc = 0;
+  CAN.read();
+  for(int i=0; i < readindices_length; i++) {
+    send_sdo_upload(read_indices[i]);
+    delay(5);
+
+    while(!CAN.available());
+    while(CAN.available()) {
+      receive_sdo_upload();
+    }
+    delay(5);
+  }
+
+  for(int i=0; i < readindices_length; i++) {
+    Serial.print(readindices_values[i]);
+    Serial.print(", ");
+  }
+
+  max_rpm = readindices_values[0];
+  deadband_voltage = readindices_values[7];
+  map_setting = readindices_values[8];
+  voltage_max = readindices_values[9];
+  offset = readindices_values[10];
+
+  if(max_rpm == -1 || deadband_voltage == -1 || map_setting == -1 || voltage_max == -1 || offset == -1) {
+    Serial.println("All parameters not read");
+    while(true);
+  }
+  
+}
+
+
+void setup() { 
+  pinMode(A0, OUTPUT);
+  Serial.begin(115200);
+
+  if (!CAN.begin(CAN_BITRATE))
+  {
+    Serial.println("CAN.begin(...) failed.");
+    for (;;) {}
+  }
+  delay(2000);
+  Serial.println();
+
+  Serial.println("Setting up parameters");
+  clear_receive_buffer();
+  if(int rc = sdo_download(OD_AccelRate, 1000) < 0) {     
+    Serial.println("SDO download failed");
+    for(;;) {}
+  }
+  delay(10);
+  if(int rc = sdo_download(OD_DecelRate, 100) < 0) {     
+    Serial.println("SDO download failed");
+    for(;;) {}
+  }
+  delay(10);
+  if(int rc = sdo_download(OD_BrakeRate, 100) < 0) {     
+  Serial.println("SDO download failed");
+  for(;;) {}
+  }
+
+  delay(2000);
+  Serial.println();
+  Serial.println("1: Read parameters");
+  Serial.println("2: Start test");
+
+  int choice;
+
+  while(Serial.available() <= 0);
+  if(Serial.available()>0) {
+    choice = Serial.parseInt();
+    Serial.parseInt();
+  }
+  
+  switch(choice) {
+    case 1:
+    delay(1000);
+    read_params();
+    while(true);
+    break;
+    
+    case 2:
+    delay(1000);
+    read_params();
+
+    Serial.println();
+    Serial.println("Enter desired throttle pot (between 0 and 5V)");
+    while(Serial.available()<=0);
+    desired_voltage = Serial.parseFloat();
+    Serial.parseFloat();
+    delay(1000);
+    Serial.println("Starting test");
+    delay(5000);
+    break;
+
+    default:
+    Serial.println("Invalid choice");
+    while(true);
+  }
+  
+}
+
+//Confirm if all measured and used values belong to the correct index in the list
+void loop() {
+  int rc = 0;
+
+  for(int i=0; i < monitorindices_length; i++) {
+    send_sdo_upload(monitor_indices[i]);
+    delay(5);
+    
+    while(!CAN.available());
+    while(CAN.available()) {
+      receive_sdo_upload_monitor();
+      delay(5);
+    }
+  }
+  
+  // while(CAN.available()) {
+  //   receive_sdo_upload_monitor();
+  //   // delay(10);
+  // }
+
+  for(int i=0; i < monitorindices_length; i++) {
+      Serial.print(monitorindices_values[i]);
+      Serial.print(",");
+  }
+  Serial.print(0.001*millis());
+  Serial.println();
+
+  //Confirm if all measured and used values belong to the correct index in the list
+  
+  if (start_time == 0xFFFFFFFF) {
+    start_time = millis();
+  }
+
+  if(millis() - start_time <= test_time) {
+    if(desired_voltage < 0.5){
+      desired_voltage = 0.5;
+    }
+    else if(desired_voltage > 4.5){
+      desired_voltage = 4.5;
+    }
+    int dc = (int) 255 * (desired_voltage - 0.5)/ (4.0); // Giving Voltage as input
+
+    analogWrite(A0, dc);
+  }
+
+  else {
+    targetReached = true;
+
+    if(reverseTest) {
+      if(monitorindices_values[0] > 100) {
+        analogWrite(A0, 255 * (reverse_voltage -0.5) / 4);
+      }
+      else {
+        analogWrite(A0, 0);
+        exit(0);
+      }
+    }
+    else {
+      analogWrite(A0, 0);
+      exit(0);
+    }
+  }
+}
+    break;
+    
+    case 2:
+    delay(1000);
+    read_params();
+
+    Serial.println();
+    Serial.println("Enter desired throttle pot (between 0 and 5V)");
+    while(Serial.available()<=0);
+    desired_voltage = Serial.parseFloat();
+    Serial.parseFloat();
+    delay(1000);
+    Serial.println("Starting test");
+    delay(5000);
+    break;
+
+    default:
+    Serial.println("Invalid choice");
+    while(true);
+  }
+  
+}
+
+//Confirm if all measured and used values belong to the correct index in the list
+void loop() {
+  int rc = 0;
+
+  for(int i=0; i < monitorindices_length; i++) {
+    send_sdo_upload(monitor_indices[i]);
+    delay(5);
+    
+    while(!CAN.available());
+    while(CAN.available()) {
+      receive_sdo_upload_monitor();
+      delay(5);
+    }
+  }
+  
+  // while(CAN.available()) {
+  //   receive_sdo_upload_monitor();
+  //   // delay(10);
+  // }
+
+  for(int i=0; i < monitorindices_length; i++) {
+      Serial.print(monitorindices_values[i]);
+      Serial.print(",");
+  }
+  Serial.print(0.001*millis());
+  Serial.println();
+
+  //Confirm if all measured and used values belong to the correct index in the list
+  
+  if (start_time == 0xFFFFFFFF) {
+    start_time = millis();
+  }
+
+  if(millis() - start_time <= test_time) {
+    if(desired_voltage < 0.5){
+      desired_voltage = 0.5;
+    }
+    else if(desired_voltage > 4.5){
+      desired_voltage = 4.5;
+    }
+    int dc = (int) 255 * (desired_voltage - 0.5)/ (4.0); // Giving Voltage as input
+
+    analogWrite(A0, dc);
+  }
+
+  else {
+    targetReached = true;
+
+    if(reverseTest) {
+      if(monitorindices_values[0] > 100) {
+        analogWrite(A0, 255 * (reverse_voltage -0.5) / 4);
+      }
+      else {
+        analogWrite(A0, 0);
+        exit(0);
+      }
+    }
+    else {
+      analogWrite(A0, 0);
+      exit(0);
+    }
+  }
+}
