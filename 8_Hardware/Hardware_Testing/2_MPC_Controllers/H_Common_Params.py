c85650553359291ab68d315c4a7ee01cfb58ccb8
Kannikeshwwar K <kannikeshwwark@gmail.com>
2025-04-07 15:12:41 +0530

    d

--- 8_Hardware/Hardware_Testing/2_MPC_Controllers/H_Common_Params.py
+++ 8_Hardware/Hardware_Testing/2_MPC_Controllers/H_Common_Params.py
@@ -3,8 +3,8 @@ import pickle
 import numpy as np
 from scipy.interpolate import Rbf
 
-main_internal_PDC_dir = os.path.dirname(os.path.abspath(__file__))
-main_internal_data_dir = os.path.abspath(os.path.join(main_internal_PDC_dir, 'data'))
+main_internal_MPC_dir = os.path.dirname(os.path.abspath(__file__))
+main_internal_data_dir = os.path.abspath(os.path.join(main_internal_MPC_dir, 'data'))
 
 # The parameters for the PD Controller
 
