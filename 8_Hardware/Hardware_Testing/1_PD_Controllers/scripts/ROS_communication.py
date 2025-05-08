4b79021f2a3297ccf3d42e9f90d72d17e4f8422d
Santhosh_Kotha <74971444+Santhosh4888@users.noreply.github.com>
2025-05-08 13:30:45 +0530

    Update ROS_communication.py

--- 8_Hardware/Hardware_Testing/1_PD_Controllers/scripts/ROS_communication.py
+++ 8_Hardware/Hardware_Testing/1_PD_Controllers/scripts/ROS_communication.py
@@ -15,16 +15,19 @@ class Communication:
         self.ego_vel = 0.0                                                                       # Velocity of ego vehicle in m/s
         self.ego_pos = 0.0                                                                       # Position of ego vehicle in meters, estimated for now, once LIDAR sensor is ready, this will be obtained directly from it
         self.obs_vel = 0.0                                                                       # Velocity of obstacle vehicle in m/s, given for now, once RADAR sensor is ready, this will be obtained directly from it 
-        self.obs_pos = 100.0                                                                     # Position of obstacle vehicle in meters, given for now, once LIDAT sensor is ready, this will be obtained directly from it
+        self.obs_pos = 100.0  
+        self.Threshold = 90.0                                                                   # Position of obstacle vehicle in meters, given for now, once LIDAT sensor is ready, this will be obtained directly from it
         self.start_time = None
         self.cur_time = None
         self.VLC = LC.VLC()                                                                      # Getting the vehicle Longitudinal controller
         self.longitudinal_control_pub = None
         self.store_position = [self.ego_pos]
         self.store_velocity = [self.ego_vel]
+        self.store_obs_pos = [self.obs_pos]
+        self.store_obs_vel = [self.obs_vel]
         self.store_time = []
         self.save_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
-        self.file_path = os.path.join(self.save_dir, 'data_may_7_pdc_1.csv')
+        self.file_path = os.path.join(self.save_dir, 'data_may_8_pdc_2_S3_05.csv')
         
     def start_vehicle(self):
         
@@ -57,10 +60,18 @@ class Communication:
         self.cur_time = rospy.Time.now()
         self.store_position.append(self.ego_pos)
         self.store_velocity.append(self.ego_vel)
+        self.store_obs_pos.append(self.obs_pos)
+        self.store_obs_vel.append(self.obs_vel)
         self.store_time.append((self.cur_time - self.start_time).to_sec())
-        self.VLC.get_control_action([self.ego_pos, self.ego_vel], [self.obs_pos, self.obs_vel])
+        if self.obs_pos - self.ego_pos < self.Threshold:
+            self.VLC.get_control_action([self.ego_pos, self.ego_vel], [self.obs_pos, self.obs_vel])
+        else:
+            self.VLC.get_control_action([self.ego_pos, self.ego_vel], None)
+
         rospy.loginfo(f'The control signal is : {self.VLC.throttle_pot} V')
         rospy.loginfo(f"data :  {self.store_position[-1]},{self.store_velocity[-1]}, {self.store_time[-1]}")
+        rospy.loginfo(f"Obstacle position : {self.store_obs_pos[-1]}, Obstacle velocity : {self.store_obs_vel[-1]}")
+        rospy.loginfo(f"separation : {self.store_obs_pos[-1] - self.store_position[-1]}")
         if self.obs_pos >= self.ego_pos + CP.Dd:
             self.longitudinal_control_pub.publish(self.VLC.throttle_pot)
         else:
@@ -73,15 +84,16 @@ class Communication:
             self.obs_msg.data = False
             self.brake_control_pub.publish(self.obs_msg.data)
         
-    def vehicle_shutdown_callback(self):
+    def vehicle_shutdown_callback(self, event):
         rospy.loginfo(f'{self.store_position}')
         
         with open(self.file_path, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["Ego_Position(m)","Ego_Velocity(m/s)", "Time(s)"])
-            writer.writerows([[pos, vel, time] for pos, vel, time in zip(self.store_position, self.store_velocity, self.store_time)])  # Saves as columns
+            writer.writerow(["Ego_Position(m)","Ego_Velocity(m/s)", "Obs_Position(m)", "Obs_Velocity(m)", "Time(s)"])
+            writer.writerows([[pos, vel, obs_pos, obs_vel, time] for pos, vel, obs_pos, obs_vel, time in zip(self.store_position, self.store_velocity, self.store_obs_pos, self.store_obs_vel, self.store_time)])  # Saves as columns
         rospy.loginfo('The test is over, vehicle is shutting down')
         rospy.signal_shutdown('Shutting down .....') 
+        
         
     def emergency_break(self):
         
